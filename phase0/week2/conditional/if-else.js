53b86e05fc98e4b1b3515a550b6f0225d9bf27b6
adamfeb <adamfeb.8@gmail.com>
2019-03-29 11:57:04 +0700

    update jawaban if-else

--- phase0/week2/conditional/if-else.js
+++ phase0/week2/conditional/if-else.js
@@ -3,21 +3,25 @@ var nama = 'AdamFeb'
 // Ada tiga peran yang kamu bisa pilih : Ksatria / Tabib / Penyihir
 // Pastikan kamu menuliskan peran dengan benar
 // Isikan peran yang kamu pilih di dalam tanda petik
-var peran = 'PeNyiHir'
+var peran = 'peNyiHir'
 peran = peran.toLowerCase()
 
-if (nama === '' && peran === '') {
-  console.log('\"Nama dan peran harus diisi!\"')
-} else if (nama === '') {
-  console.log('\"Kamu telah memilih peran ' + peran + '.\"\n\"Tetapi kamu harus mengisi nama terlebih dahulu untuk memulai game!\"')
-} else if (peran === '') {
-  console.log('\"Halo ' + nama + ', Kamu harus memilih peranmu untuk memulai game!\"')
-} else if (peran === 'ksatria') {
-  console.log('\"Selamat datang di Dunia Proxytia, ' + nama + '\"\n\"Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!\"')
-} else if (peran === 'tabib') {
-  console.log('\"Selamat datang di Dunia Proxytia, ' + nama + '\"\n\"Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.\"')
-} else if (peran === 'penyihir') {
-  console.log('\"Selamat datang di Dunia Proxytia, ' + nama + '\"\n\"Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!\"')
+if (nama !== '' && peran !== '') {
+  if (peran === 'ksatria') {
+    console.log('\"Selamat datang di Dunia Proxytia, ' + nama + '\"\n\"Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!\"')
+  } else if (peran === 'tabib') {
+    console.log('\"Selamat datang di Dunia Proxytia, ' + nama + '\"\n\"Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.\"')
+  } else if (peran === 'penyihir') {
+    console.log('\"Selamat datang di Dunia Proxytia, ' + nama + '\"\n\"Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!\"')
+  } else {
+    console.log('\"Maaf ' + nama + ', Peran yang kamu tuliskan tidak tersedia di game ini.\"')
+  }
 } else {
-  console.log('\"Maaf ' + nama + ', Peran yang kamu tuliskan tidak tersedia di game ini.\"')
+  if (nama === '' && peran === '') {
+    console.log('\"Untuk memulai game, Nama dan Peran harus diisi!\"')
+  } else if (peran === '') {
+    console.log('\"Halo ' + nama + ', Kamu harus memilih peranmu untuk memulai game!\"')
+  } else {
+    console.log('\"Kamu telah memilih peran ' + peran + ', tetapi kamu harus mengisi nama terlebih dahulu untuk memulai game!\"')
+  }
 }
