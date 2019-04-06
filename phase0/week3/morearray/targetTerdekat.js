ce3b21b44c9533d0fe9373dc8215087905148e3e
adamfeb <adamfeb.8@gmail.com>
2019-04-06 10:00:44 +0700

    logic lainnya

--- phase0/week3/morearray/targetTerdekat.js
+++ phase0/week3/morearray/targetTerdekat.js
@@ -1,26 +1,26 @@
 function targetTerdekat (arr) {
-  var jarak = arr.length
-  var checkx = false
-  var checko = false
-  for (var h = 0; h < arr.length; h++) {
-    if (arr[h] === 'x') {
-      checkx = true
-    } else if (arr[h] === 'o') {
-      checko = true
+  var indexOfx = []
+  var indexOfo = []
+  var shortest = arr.length
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === 'x') {
+      indexOfx.push(i)
+    } else if (arr[i] === 'o') {
+      indexOfo.push(i)
     }
   }
-  if (checkx === true && checko === true) {
-    for (var i = 0; i < arr.length; i++) {
-      for (var j = 0; j < arr.length; j++) {
-        if (((arr[i] === 'o' && arr[j] === 'x') || (arr[i] === 'x' && arr[j] === 'o')) && Math.abs(j - i) < jarak) {
-          jarak = Math.abs(j - i)
+  if (indexOfx.length !== 0 && indexOfo.length !== 0) {
+    for (var p = 0; p < indexOfx.length; p++) {
+      for (var q = 0; q < indexOfo.length; q++) {
+        if (Math.abs(indexOfx[p] - indexOfo[q]) <= shortest) {
+          shortest = Math.abs(indexOfx[p] - indexOfo[q])
         }
       }
     }
+    return shortest
   } else {
-    jarak = 0
+    return 0
   }
-  return jarak
 }
 
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])) // 3
@@ -29,7 +29,81 @@ console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])) // 1
 console.log(targetTerdekat([' ', ' ', 'o', ' '])) // 0
 console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])) // 2
 
-// Versi Panjang dan Mudahnya
+// SETELAH KONSULTASI DENGAN INSTRUKTUR - VERSI PANJANG DAN MUDAH DIPAHAMI
+
+// function targetTerdekat (arr) {
+//   var indexOfx = []
+//   var indexOfo = []
+//   var shortest = arr.length
+
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === 'x') {
+//       indexOfx.push(i)
+//       console.log(indexOfx)
+//     } else if (arr[i] === 'o') {
+//       indexOfo.push(i)
+//       console.log(indexOfo)
+//     }
+//   }
+//   console.log ('------')
+//   console.log (indexOfx + ' --- ' + indexOfx.length)
+//   console.log (indexOfo + ' --- ' + indexOfo.length)
+
+//   if (indexOfx.length !== 0 && indexOfo.length !== 0) {
+//     for (var p = 0; p < indexOfx.length; p++) {
+//       for (var q = 0; q < indexOfo.length; q++) {
+//         console.log('x = ' + indexOfx[p] + ' --- o = ' + indexOfo[q] + ' jarak = ' + Math.abs(indexOfx[p] - indexOfo[q]))
+//         if (Math.abs(indexOfx[p] - indexOfo[q]) <= shortest) {
+//           shortest = Math.abs(indexOfx[p] - indexOfo[q])
+//           console.log(shortest)
+//         }
+//       }
+//     }
+//   } else {
+//     return 0
+//   }
+// }
+
+// console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])) // 3
+// console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])) // 4
+// console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])) // 1
+// console.log(targetTerdekat([' ', ' ', 'o', ' '])) // 0
+// console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])) // 2
+
+// VERSI PENDEKNYA
+
+// function targetTerdekat (arr) {
+//   var jarak = arr.length
+//   var checkx = false
+//   var checko = false
+//   for (var h = 0; h < arr.length; h++) {
+//     if (arr[h] === 'x') {
+//       checkx = true
+//     } else if (arr[h] === 'o') {
+//       checko = true
+//     }
+//   }
+//   if (checkx === true && checko === true) {
+//     for (var i = 0; i < arr.length; i++) {
+//       for (var j = 0; j < arr.length; j++) {
+//         if (((arr[i] === 'o' && arr[j] === 'x') || (arr[i] === 'x' && arr[j] === 'o')) && Math.abs(j - i) < jarak) {
+//           jarak = Math.abs(j - i)
+//         }
+//       }
+//     }
+//   } else {
+//     jarak = 0
+//   }
+//   return jarak
+// }
+
+// console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])) // 3
+// console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])) // 4
+// console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])) // 1
+// console.log(targetTerdekat([' ', ' ', 'o', ' '])) // 0
+// console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])) // 2
+
+// VERSI PANJANG DAN MUDAH DIBACANYA
 
 // function targetTerdekat (arr) {
 //   var jarak = arr.length
