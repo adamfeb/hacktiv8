bd53736eb401b518737702c84525bd0e519898bf
adamfeb <adamfeb.8@gmail.com>
2019-04-09 01:30:08 +0700

    update jawaban

---
+++ phase0/week4/livecoding/rocket.js
@@ -0,0 +1,23 @@
+function countAnagramDeletion(stringA, stringB) {
+  // kode anda disini
+}
+
+console.log(countAnagramDeletion('cde', 'abc')); // 4
+
+console.log(
+  countAnagramDeletion('ayam', 'maya')
+); // 0
+
+// console.log(
+//   countAnagramDeletion(
+//     'bugexikjevtubidpulaelsbcqlupwetzyzdvjphn',
+//     'lajoipfecfinxjspxmevqxuqyalhrsxcvgsdxxkacspbchrbvvwnvsdtsrdk'
+//   )
+// ); // 40
+
+// console.log(
+//   countAnagramDeletion(
+//     'imkhnpqnhlvaxlmrsskbyyrhwfvgteubrelgubvdmrdmesfxkpykprunzpustowmvhupkqsyjxmnptkcilmzcinbzjwvxshubeln',
+//     'wfnfdassvfugqjfuruwrdumdmvxpbjcxorettxmpcivurcolxmeagsdundjronoehtyaskpwumqmpgzmtdmbvsykxhblxspgnpgfzydukvizbhlwmaajuytrhxeepvmcltjmroibjsdkbqjnqjwmhsfopjvehhiuctgthrxqjaclqnyjwxxfpdueorkvaspdnywupvmy'
+//   )
+// ); // 102
